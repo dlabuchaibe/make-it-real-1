998ec79370458fefabe110ee1b2bb606bda4aed3
Jesus Estrada <jesuestrada@gmail.com>
2020-04-28 21:16:09 -0500

    comments

--- 07-nodeAPIRestUserAuth/index.js
+++ 07-nodeAPIRestUserAuth/index.js
@@ -9,8 +9,8 @@ const config = require('./config');
 const app = express();
 
 //variables
-let users = [];
-let tokens = [];
+const users = [];
+const tokens = [];
 
 //functions
 const listUsers = (users)=> {
@@ -28,10 +28,14 @@ const logger = (req, res, next) => {
     next();
 };
 const auth = (req, res, next) => {
+    //obtener el token de los headers de la petición
     const token = req.headers['token'];
+    //buscar en el arreglo tokens el token obtenido anteriormente
     tokens.includes(token) ? 
+        //si la respuesta es válida, continuar
         next()
     :
+        //si la respuesta no es válida, responder un mensaje de error
         res.status(500).send('Usuario no autorizado')
 
 };
@@ -39,49 +43,57 @@ const auth = (req, res, next) => {
 //config
 app.use(nocache());
 app.use(bodyParser.json());
+//usar el middelware logger en todas las peticiones
 app.use(logger);
 
 //routes
-app.get('/', (req, res)=>{
-    res
-    .sendStatus(500);
-});
-app.get('/users', auth, (req, res)=>{
-    res
-    .status(200)
-    .send(`Usuarios: ${listUsers(users)}`);
-});
-app.get('/users/:id', auth, (req, res)=>{
-    res
-    .status(200)
-    .send(`El usuario en la posición ${id} es ${users[id].username}`)
-});
-app.post('/users',(req, res)=>{
-    let user = {
-        username: req.body.username,
-        password: req.body.password
-    };
-    users.push(user);
-    res
-    .status(200)
-    .send(`El usuario ${user.username} fue creado`);
-});
-app.post('/users/login', (req, res)=>{
-    const username = req.body.username;
-    const password = req.body.password;
-    //buscar el usuario a autenticar en el objeto users
-    if(!!users.find(user => user.username === username && user.password === password)){   
-        const r = Math.random();
-        tokens.push(r.toString());
+    //principal
+    app.get('/', (req, res)=>{
+        res
+        .sendStatus(500);
+    });
+    //privadas (usan middleware de autenticación)
+    app.get('/users', auth, (req, res)=>{
         res
         .status(200)
-        .send(`{token: ${r} }`);
-    }else{    
+        .send(`Usuarios: ${listUsers(users)}`);
+    });
+    app.get('/users/:id', auth, (req, res)=>{
         res
-        .status(500)
-        .send(`Datos no válidos`);
-    }    
-});
+        .status(200)
+        .send(`El usuario en la posición ${id} es ${users[id].username}`)
+    });
+    //públicas (sin middleware)
+    app.post('/users',(req, res)=>{
+        let user = {
+            username: req.body.username,
+            password: req.body.password
+        };
+        //agregar el usuario al arreglo users (mejora futura: validar que el mismo username no exista)
+        users.push(user);
+        res
+        .status(200)
+        .send(`El usuario ${user.username} fue creado`);
+    });
+    app.post('/users/login', (req, res)=>{
+        const username = req.body.username;
+        const password = req.body.password;
+        //buscar el usuario a autenticar en el arreglo users
+        if(!!users.find(user => user.username === username && user.password === password)){   
+            //generar un token aleatorio
+            const r = Math.random();
+            //guardar el token en el arreglo tokens
+            tokens.push(r.toString());
+            res
+            .status(200)
+            //enviar como respuesta el token 
+            .send(`{token: ${r} }`);
+        }else{    
+            res
+            .status(500)
+            .send(`Datos no válidos`);
+        }    
+    });
 
 //server
 app.listen(config.port, ()=>{
