33f1f50b5d9876adf3ff6461b23df6614994ca18
Jesus Estrada <jesuestrada@gmail.com>
2020-06-09 20:06:39 -0500

    check login + validations

--- 11-mongodb/public/main.js
+++ 11-mongodb/public/main.js
@@ -1,3 +1,14 @@
+const checkLogin = () => {
+    const token = localStorage.getItem('token');
+    if(token === null){
+        document.getElementById('card-signup').style.display = 'block';
+        document.getElementById('card-login').style.display = 'block';
+    }else{
+        document.getElementById('card-tweets').style.display = 'block';
+        document.getElementById('logout').style.display = 'block';
+    }
+};
+
 const login = () => {
     const url = '/api/users/login';
     const user = {
@@ -16,6 +27,7 @@ const login = () => {
     .then(response=>{
         if(!!response.token){
             document.getElementById('login-username').value = '';
+            document.getElementById('logout').style.display = 'block';
             document.getElementById('card-tweets').style.display = 'block';
             document.getElementById('card-login').style.display = 'none';
             document.getElementById('card-signup').style.display = 'none';
@@ -30,34 +42,73 @@ const login = () => {
     })
 };
 
-const newUser = () => {
-    console.log('newUser');
-    const url = '/api/users';
-    const user = {
-        name: document.getElementById('name').value,
-        email: document.getElementById('email').value,
-        username: document.getElementById('username').value,
-        password: document.getElementById('password').value
+const logout = () => {
+    localStorage.removeItem('token');
+    document.getElementById('logout').style.display = 'none';
+    document.getElementById('card-tweets').style.display = 'none';
+    document.getElementById('card-signup').style.display = 'block';
+    document.getElementById('card-login').style.display = 'block';
+};
+
+const validateNewUser = (name, email, username, password, passwordConfirm) => {
+    if(name.length<2){
+        alert("El nombre debe ser mínimo de 2 caracteres");
+        return false;
     }
-    document.getElementById('password').value = '';
-    document.getElementById('passwordConfirm').value = '';
-    fetch(url, {
-        method: 'POST',
-        body: JSON.stringify(user), 
-        headers:{
-            'Content-Type': 'application/json'
+    if(!/^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,3})+$/.test(email)){
+        alert("No es una dirección de correo válida");
+        return false;
+    }
+    if(username.length<2){
+        alert("El nombre de usuario debe ser mínimo de 2 caracteres");
+        return false;
+    }
+    if(password != passwordConfirm){
+        alert("Las contraseñas no coinciden");
+        return false;
+    }
+    if(!/^[A-Za-z]\w{7,14}$/.test(password)){
+        alert("La contraseña no es válida");
+        return false;
+    }
+
+    return true;
+}
+
+const newUser = () => {
+    if(validateNewUser(document.getElementById('name').value,
+                        document.getElementById('email').value,
+                        document.getElementById('username').value,
+                        document.getElementById('password').value,
+                        document.getElementById('passwordConfirm').value
+    )){
+        const url = '/api/users';
+        const user = {
+            name: document.getElementById('name').value,
+            email: document.getElementById('email').value,
+            username: document.getElementById('username').value,
+            password: document.getElementById('password').value
         }
-    })
-    .then(res=> res.json())
-    .then(response=>{
-        document.getElementById('name').value = '';
-        document.getElementById('email').value = '';
-        document.getElementById('username').value = '';
-        alert('Usuario creado!');
-    })
-    .catch(err=>{
-        alert('Ocurrió un error al crear el usurio');
-    })
+        document.getElementById('password').value = '';
+        document.getElementById('passwordConfirm').value = '';
+        fetch(url, {
+            method: 'POST',
+            body: JSON.stringify(user), 
+            headers:{
+                'Content-Type': 'application/json'
+            }
+        })
+        .then(res=> res.json())
+        .then(response=>{
+            document.getElementById('name').value = '';
+            document.getElementById('email').value = '';
+            document.getElementById('username').value = '';
+            alert('Usuario creado!');
+        })
+        .catch(err=>{
+            alert('Ocurrió un error al crear el usurio');
+        })
+    }
 };
 
 const newTweet = () => {
