a0f49dc8a18af666c7b3634364758592a48f5cda
Jesus Estrada <jesuestrada@gmail.com>
2020-06-17 16:39:49 -0500

    carousel buttons

--- 12-Jquery/public/js/main-jquery.js
+++ 12-Jquery/public/js/main-jquery.js
@@ -2,7 +2,7 @@ $(document).ready(() => {
     getWeather('Barranquilla');
     checkLogin();
     getTweets();
-    $('.jcarousel').jcarousel();
+    
     $('#logout').click(()=>{
         logout();
     });
@@ -12,7 +12,40 @@ $(document).ready(() => {
             password: $('#login-password').val()
         };
         login(user);
-    })
+    });
+    
+    $('.jcarousel').jcarousel();
+
+    $('.jcarousel-control-prev')
+            .on('jcarouselcontrol:active', function() {
+                $(this).removeClass('inactive');
+            })
+            .on('jcarouselcontrol:inactive', function() {
+                $(this).addClass('inactive');
+            })
+            .jcarouselControl({
+                target: '-=1'
+            });
+
+        $('.jcarousel-control-next')
+            .on('jcarouselcontrol:active', function() {
+                $(this).removeClass('inactive');
+            })
+            .on('jcarouselcontrol:inactive', function() {
+                $(this).addClass('inactive');
+            })
+            .jcarouselControl({
+                target: '+=1'
+            });
+
+        $('.jcarousel-pagination')
+            .on('jcarouselpagination:active', 'a', function() {
+                $(this).addClass('active');
+            })
+            .on('jcarouselpagination:inactive', 'a', function() {
+                $(this).removeClass('active');
+            })
+            .jcarouselPagination();
 });
 
 const getWeather = (city) => {
