09e9586a930e17bab6cce918b121052324c23f75
Jesus Estrada <jesuestrada@gmail.com>
2020-07-22 22:36:15 -0500

    paginate

--- 13-react/backend/api/controllers/tweets/index.js
+++ 13-react/backend/api/controllers/tweets/index.js
@@ -1,107 +1,119 @@
-const express = require('express');
+const express = require("express");
 const router = express.Router();
-const Tweet = require('./../../models/tweets');
-const User = require('./../../models/users');
-const auth  = require('./../../middlewares/auth');
+const paginate = require("express-paginate");
+const Tweet = require("./../../models/tweets");
+const User = require("./../../models/users");
+const auth = require("./../../middlewares/auth");
 
+router
+  .route("/")
+  .get(async (req, res) => {
+    const { page = 1, limit = 10 } = req.query;
+    const tweets = await Tweet.find({}, ["content", "image", "createdAt", "likes"])
+                        .populate("user", ["username"])
+                        .populate("comments.userId", ["username"])
+                        .sort({ createdAt: -1 })
+                        .limit(limit * 1)
+                        .skip((page - 1) * limit)
+                        .exec();
 
-router.route('/')
-    .get((req, res)=>{
-        Tweet.find({})
-            .populate('user', ["username"])
-            .populate('comments.userId', ["username"])
-            .sort({createdAt: -1})
-            .then(response=>{
-                res.status(200).send(response); 
-            })
-            .catch(err=>{
-                res.status(500).send("ocurrió un error"); 
-            })
-    })
-    .post(auth, (req, res)=>{
-        //crear el objeto que se va a guardar
-        const content = req.body.content;
-        const image = req.body.image;
-        const user = req._id;
-        const likes = 0;
-        const tweet = {
-            content,
-            image,
-            user,
-            likes
-        };
-        Tweet.find({content: tweet.content})
-        .then(tweets=>{
-            if(tweets.length>0){
-                res.status(500).send({message: 'Ya existe un elemento con el mismo contenido'});
-            }else{
-                const object = new Tweet(tweet);
-                object.save()
-                .then(()=>{
-                    res.status(200).send({message: 'El tweet ha sido creado'});
-                });
-            }    
+    const count = await Tweet.countDocuments();
+    const pageCount = Math.ceil(count / req.query.limit);
+
+    res.json({
+      tweets,
+      totalPages: Math.ceil(count / limit),
+      currentPage: page,
+      has_more: paginate.hasNextPages(req)(pageCount),
+    });
+  })
+  .post(auth, (req, res) => {
+    //crear el objeto que se va a guardar
+    const content = req.body.content;
+    const image = req.body.image;
+    const user = req._id;
+    const likes = 0;
+    const tweet = {
+      content,
+      image,
+      user,
+      likes,
+    };
+    Tweet.find({ content: tweet.content }).then((tweets) => {
+      if (tweets.length > 0) {
+        res
+          .status(500)
+          .send({ message: "Ya existe un elemento con el mismo contenido" });
+      } else {
+        const object = new Tweet(tweet);
+        object.save().then(() => {
+          res.status(200).send({ message: "El tweet ha sido creado" });
         });
-
-        
-    })
-    .put(auth, (req, res)=>{
-        const tweet = {
-            id: req.body.id,
-            content: req.body.content
-        };
-        Tweet.update({id: tweet.id}, {content: tweet.content})
-        .then(()=>{
-            res.status(200).send({message: 'El elemento fue actualizado'});
-        });
-
-        res.status(200).send({message: 'El tweet ha sido actualizado'});
-    })
-    .delete(auth, (req, res)=>{
-        Tweet.remove({})
-        .then(()=>{
-            res.status(200).send({message: 'Todos los tweets han sido eliminados'});
-        });
+      }
+    });
+  })
+  .put(auth, (req, res) => {
+    const tweet = {
+      id: req.body.id,
+      content: req.body.content,
+    };
+    Tweet.update({ id: tweet.id }, { content: tweet.content }).then(() => {
+      res.status(200).send({ message: "El elemento fue actualizado" });
     });
 
-    router.route('/like')
-    .post(auth, (req, res)=>{
-        const id = req.body.id;
-        Tweet.update({_id: id}, { $inc: {likes: 1}})
-        .then((response)=>{
-            res.sendStatus(200);
+    res.status(200).send({ message: "El tweet ha sido actualizado" });
+  })
+  .delete(auth, (req, res) => {
+    Tweet.remove({}).then(() => {
+      res.status(200).send({ message: "Todos los tweets han sido eliminados" });
+    });
+  });
+
+router.route("/like").post(auth, (req, res) => {
+  const id = req.body.id;
+  Tweet.update({ _id: id }, { $inc: { likes: 1 } }).then((response) => {
+    res.sendStatus(200);
+  });
+});
+
+router
+  .route("/:username")
+  .get((req, res) => {
+    const username = req.params.username;
+    User.find({ username: username })
+      .then(async (user) => {
+        userId = user[0]._id;
+            const { page = 1, limit = 10 } = req.query;
+            const tweets = await Tweet.find({ user: userId }, ["content", "image", "createdAt", "likes"])
+                                .populate("user", ["username"])
+                                .populate("comments.userId", ["username"])
+                                .sort({ createdAt: -1 })
+                                .limit(limit * 1)
+                                .skip((page - 1) * limit)
+                                .exec();
+
+            const count = await Tweet.countDocuments();
+            const pageCount = Math.ceil(count / req.query.limit);
+
+            res.json({
+            tweets,
+            totalPages: Math.ceil(count / limit),
+            currentPage: page,
+            has_more: paginate.hasNextPages(req)(pageCount),
+            });
         })
+    .catch((err) => {
+      res.status(400).send({ message: "No existe el usuario" });
     });
 
-router.route('/:username')
-    .get((req, res)=>{
-        const username = req.params.username;
-        User.find({username: username})
-        .then(user=>{
-            userId = user[0]._id
-            Tweet.find({user: userId})
-                .populate('user', ["username"])
-                .populate('comments.userId', ["username"])
-                .sort({createdAt: -1})
-                .then(response=>{
-                    res.status(200).send(response); 
-                })
-                .catch(err=>{
-                    res.status(500).send("ocurrió un error"); 
-                })
-        })
-        .catch(err=>{
-            res.status(400).send({message: 'No existe el usuario'});
-        })
-
-        
-    })
-    .delete(auth, (req, res)=>{
-        const id = req.params.id;
-        Tweet.remove({_id: id})
-        .then(()=>{
-            res.status(200).send({message: `El elemento con id: ${id} ha sido eliminado`});
-        });
+  })
+  .delete(auth, (req, res) => {
+    const id = req.params.id;
+    Tweet.remove({ _id: id }).then(() => {
+      res
+        .status(200)
+        .send({ message: `El elemento con id: ${id} ha sido eliminado` });
     });
+  });
 
 module.exports = router;
--- 13-react/backend/index.js
+++ 13-react/backend/index.js
@@ -1,6 +1,7 @@
 const express = require('express');
 const mongoose = require('mongoose');
 const cors = require('cors');
+const paginate = require('express-paginate');
 const dotenv = require('dotenv').config()
 
 const app = express();
@@ -9,6 +10,7 @@ const api = require('./api');
 
 app.use(cors());
 app.use(express.json({limit: '50mb'}));
+app.use(paginate.middleware(10, 50));
 app.use('/api', api);
 app.use('/api/v1', api);
 
