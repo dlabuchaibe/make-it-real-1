29615c1213b93ef91880dbba08ab7f7dfff2af44
Jesus Estrada <jesuestrada@gmail.com>
2020-07-30 20:38:06 -0500

    test front

--- 13-react/backend/__tests__/division.test.js
+++ 13-react/backend/__tests__/division.test.js
@@ -1,8 +1,10 @@
-const division = require('./../api/utilities/division');
+const division = require('../api/utilities/division');
 
-test('test: division', ()=>{
-    expect(division(2,2)).toBe(1);
-});
-test('test: division entre cero', ()=>{
-    expect(division(2,0)).toBe("error");
+describe('division', ()=>{
+    it('exitosa', ()=>{
+        expect(division(2,2)).toBe(1);
+    });
+    it('entre cero', ()=>{
+        expect(division(2,0)).toBe("error");
+    });
 });
